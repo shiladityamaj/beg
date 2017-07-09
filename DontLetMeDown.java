c17d6f0ff49c8bd95f63470c473b13f81a2269c9
shiladityamaj <shiladityahc96@gmail.com>
2017-07-09 14:26:59 +0530

    Add files via upload

---
+++ DontLetMeDown.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class letitbe
+
+{
+ 
+   public static void main(String[]args)
+
+    {
+  
+      Scanner input=new Scanner(System.in);
+ 
+       int i=input.nextInt();
+  
+      System.out.println(i);
+     
+   if(i>0)
+     
+   {
+     
+       System.out.println("positive");
+        }
+
+        else if(i==0)
+       
+ {
+            System.out.println("zero");
+        }
+ 
+       else
+        {
+            System.out.println("negative");
+        }
+ 
+   }
+}
